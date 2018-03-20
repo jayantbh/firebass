f86e839e05b3a4306cf79ab7d146382d18e10465
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-03-21 04:20:10 +0530

    Rename fb-youtube to fb-player

--- tests/integration/components/fb-player/component-test.js
+++ tests/integration/components/fb-player/component-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | fb-youtube', function(hooks) {
+module('Integration | Component | fb-player', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{fb-youtube}}`);
+    await render(hbs`{{fb-player}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#fb-youtube}}
+      {{#fb-player}}
         template block text
-      {{/fb-youtube}}
+      {{/fb-player}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
