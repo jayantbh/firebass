c00a480143cbeddb64685b1bbafaf184177b4dc1
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-03-25 05:24:49 +0530

    Implement search and display of results using the Youtube API.

--- app/components/fb-body/component.js
+++ app/components/fb-body/component.js
@@ -1,10 +1,20 @@
 import Component from '@ember/component';
+import fetch from 'fetch';
+import ENV from 'firebass/config/environment';
+import { task, timeout } from 'ember-concurrency';
+
+const YOUTUBE_ID_REGEX = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/i;
+const DEBOUNCE_DURATION = 500;
 
 export default Component.extend({
   tagName: '',
 
-  videoUrlOrId: 'https://www.youtube.com/watch?v=oWCrc3MwXPI',
+  searchTerm: 'https://www.youtube.com/watch?v=oWCrc3MwXPI',
   videoId: '',
+  searchResults: null,
+
+  // Allow searching for either just videos, or videos and playlists, or just playlists
+  searchType: 'video', // Planned: playlist | video,playlist
 
   // store: service(),
 
@@ -15,15 +25,35 @@ export default Component.extend({
     //   body: 'You can store and sync data in realtime without a backend.'
     // });
     // newPost.save();
+    // this.get('searchYoutube').perform('veritasium').then(console.log)
   },
+
+  searchYoutube: task(function * (term) {
+    yield timeout(DEBOUNCE_DURATION);
+    return yield fetch(`https://www.googleapis.com/youtube/v3/search?key=${ENV.FIREBASS_GOOGLE_API_KEY}&type=${this.searchType}&part=snippet&q=${term}`).then(function(response) {
+      return response.json();
+    });
+  }).restartable(),
 
   actions: {
     updateVideoId(event) {
       event.preventDefault();
 
-      let videoUrlOrId = this.get('videoUrlOrId');
-      let [, videoId] = videoUrlOrId.match(/(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/i);
+      let searchTerm = this.get('searchTerm');
+      let [, videoId] = searchTerm.match(YOUTUBE_ID_REGEX);
       this.set('videoId', videoId);
+    },
+
+    handleSearchInput(value) {
+      this.set('searchTerm', value);
+      let videoId;
+      try {
+        [, videoId] = value.match(YOUTUBE_ID_REGEX);
+      } catch(e) {
+      //  do nothing
+      }
+
+      this.get('searchYoutube').perform(videoId || value).then((results) => this.set('searchResults', results)).catch(() => {});
     }
   }
 });
