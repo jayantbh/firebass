5bcd1d8c29563a38046d8aafbe45a7968bb3f9ab
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-03-26 02:25:24 +0530

    Get all persisted songs from the server at route level.

--- app/session/route.js
+++ app/session/route.js
@@ -5,5 +5,11 @@ export default Route.extend({
   session: service(),
   beforeModel: function() {
     return this.get('session').fetch().catch(function() {});
+  },
+  model() {
+    if (this.get('session.currentUser.uid')) {
+      return this.store.findAll('playable-entity');
+    }
+    return [];
   }
 });
