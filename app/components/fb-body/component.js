a11d14fc6a7b399708c80869c420be3b96b298f4
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-03-26 02:29:52 +0530

    Allow persisting of songs to server. Use ember events instead of DOM events. Fix sidebar width. Style youtube results cards and panel. Style search input bar.

--- app/components/fb-body/component.js
+++ app/components/fb-body/component.js
@@ -2,6 +2,7 @@ import Component from '@ember/component';
 import fetch from 'fetch';
 import ENV from 'firebass/config/environment';
 import { task, timeout } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
 
 const YOUTUBE_ID_REGEX = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/i;
 const DEBOUNCE_DURATION = 500;
@@ -12,33 +13,36 @@ export default Component.extend({
   searchTerm: 'https://www.youtube.com/watch?v=oWCrc3MwXPI',
   videoId: '',
   searchResults: null,
+  songs: null,
 
   // Allow searching for either just videos, or videos and playlists, or just playlists
   searchType: 'video', // Planned: playlist | video,playlist
 
-  // store: service(),
-
-  init() {
-    this._super(...arguments);
-    // var newPost = this.get('store').createRecord('post', {
-    //   title: 'EmberFire is flaming hot!',
-    //   body: 'You can store and sync data in realtime without a backend.'
-    // });
-    // newPost.save();
-    // this.get('searchYoutube').perform('veritasium').then(console.log)
-  },
+  store: service(),
 
   searchYoutube: task(function * (term) {
+    if (!term.length) return;
     yield timeout(DEBOUNCE_DURATION);
     return yield fetch(`https://www.googleapis.com/youtube/v3/search?key=${ENV.FIREBASS_GOOGLE_API_KEY}&type=${this.searchType}&part=snippet&q=${term}`).then(function(response) {
       return response.json();
     });
   }).restartable(),
 
-  actions: {
-    updateVideoId(event) {
-      event.preventDefault();
+  addSong: task(function * (video) {
+    let song = this.get('store').createRecord('playable-entity', {
+      videoId: video.id.videoId,
+      kind: video.id.kind,
+      title: video.snippet.title,
+      thumbnail: video.snippet.thumbnails.high.url || video.snippet.thumbnails.default.url,
+      publishedAt: video.snippet.publishedAt,
+      description: video.snippet.description,
+      channelTitle: video.snippet.channelTitle
+    });
+    return yield song.save();
+  }).drop(),
 
+  actions: {
+    updateVideoId() {
       let searchTerm = this.get('searchTerm');
       let [, videoId] = searchTerm.match(YOUTUBE_ID_REGEX);
       this.set('videoId', videoId);
@@ -54,6 +58,14 @@ export default Component.extend({
       }
 
       this.get('searchYoutube').perform(videoId || value).then((results) => this.set('searchResults', results)).catch(() => {});
+    },
+
+    addToQueue(video) {
+      console.log(video);
+    },
+    addToSongs(video) {
+      console.log(video);
+      this.get('addSong').perform(video).then((song) => this.get('songs').addObject(song)).catch(() => {});
     }
   }
 });
