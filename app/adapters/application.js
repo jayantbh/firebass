502196504d8f9fb29a7426ad39dbf2ef1183a34f
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-03-26 17:35:39 +0530

    Move UID in API call logic from playable-entity adapter to Application adapter.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,9 @@
 import FirebaseAdapter from 'emberfire/adapters/firebase';
+import { inject as service } from '@ember/service';
 
 export default FirebaseAdapter.extend({
+  session: service(),
+  pathForType() {
+    return this.get('session.currentUser.uid');
+  }
 });
