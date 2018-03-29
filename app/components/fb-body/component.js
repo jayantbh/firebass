a2b4a96633d329e2692db73836ab392111f1d25e
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-03-29 17:45:51 +0530

    Componentise the search functionality, and shift the relevant styles, properties, and methods as applicable.

--- app/components/fb-body/component.js
+++ app/components/fb-body/component.js
@@ -1,13 +1,9 @@
 import Component from '@ember/component';
-import fetch from 'fetch';
-import ENV from 'firebass/config/environment';
-import { task, timeout } from 'ember-concurrency';
+import { task } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
-import { ytObjectToPlayableEntityModel } from 'firebass/lib/yt-object'
-import { computed } from '@ember/object';
+import { ytObjectToPlayableEntityModel } from 'firebass/lib/yt-object';
+import EmberObject, { computed } from '@ember/object';
 
-const YOUTUBE_ID_REGEX = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/i;
-const DEBOUNCE_DURATION = 500;
 
 export default Component.extend({
   tagName: '',
@@ -18,9 +14,6 @@ export default Component.extend({
   songs: null,
   queue: null,
   playlists: () => [],
-
-  // Allow searching for either just videos, or videos and playlists, or just playlists
-  searchType: 'video', // Planned: playlist | video,playlist
 
   store: service(),
 
@@ -34,35 +27,15 @@ export default Component.extend({
     return this.get('queue.entities').objectAt(index);
   }),
 
-  searchYoutube: task(function * (term) {
-    if (!term.length) return;
-    yield timeout(DEBOUNCE_DURATION);
-    return yield fetch(`https://www.googleapis.com/youtube/v3/search?key=${ENV.FIREBASS_GOOGLE_API_KEY}&type=${this.searchType}&part=snippet&q=${term}`).then(function(response) {
-      return response.json();
-    });
-  }).restartable(),
-
   addSong: task(function * (video) {
     let song = this.get('store').createRecord('playable-entity', ytObjectToPlayableEntityModel(video));
     return yield song.save();
   }).drop(),
 
   actions: {
-    handleSearchInput(value) {
-      this.set('searchTerm', value);
-      let videoId;
-      try {
-        [, videoId] = value.match(YOUTUBE_ID_REGEX);
-      } catch(e) {
-      //  do nothing
-      }
-
-      this.get('searchYoutube').perform(videoId || value).then((results) => this.set('searchResults', results)).catch(() => {});
-    },
-
     addToList: async function (track, list) {
       let song;
-      if (!track.id) {
+      if (!(track instanceof EmberObject)) {
         song = await this.get('addSong').perform(track).catch(() => {});
       } else { song = track; }
 
---
+++ app/components/fb-search/component.js
@@ -0,0 +1,43 @@
+import Component from '@ember/component';
+import { task, timeout } from 'ember-concurrency';
+import fetch from 'fetch';
+import ENV from 'firebass/config/environment';
+
+const YOUTUBE_ID_REGEX = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/i;
+const DEBOUNCE_DURATION = 500;
+
+export default Component.extend({
+  tagName: '',
+
+  searchTerm: '',
+  searchResults: null,
+  songs: null,
+  queue: null,
+  handleSearchInput: () => {},
+  addToList: () => {},
+
+  // Allow searching for either just videos, or videos and playlists, or just playlists
+  searchType: 'video', // Planned: playlist | video,playlist
+
+  searchYoutube: task(function * (term) {
+    if (!term.length) return;
+    yield timeout(DEBOUNCE_DURATION);
+    return yield fetch(`https://www.googleapis.com/youtube/v3/search?key=${ENV.FIREBASS_GOOGLE_API_KEY}&type=${this.searchType}&part=snippet&q=${term}`).then(function(response) {
+      return response.json();
+    });
+  }).restartable(),
+
+  actions: {
+    handleSearchInput(value) {
+      this.set('searchTerm', value);
+      let videoId;
+      try {
+        [, videoId] = value.match(YOUTUBE_ID_REGEX);
+      } catch(e) {
+        //  do nothing
+      }
+
+      this.get('searchYoutube').perform(videoId || value).then((results) => this.set('searchResults', results)).catch(() => {});
+    }
+  }
+});
