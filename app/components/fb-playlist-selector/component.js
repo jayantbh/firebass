9c2952befb32028df62ac2ca66b4af6a92bd040c
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-03-31 03:17:16 +0530

    Use a computed property to filter playlists that already contain a certain song.

--- app/components/fb-playlist-selector/component.js
+++ app/components/fb-playlist-selector/component.js
@@ -1,12 +1,21 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: '',
   horizontalPosition: 'left',
   verticalPosition: 'below',
+  filterOutSelectedPlaylists: false,
   isAbsolute: false,
   playlist: null,
   playlists: null,
   result: null,
-  addToList: () => {}
+  addToList: () => {},
+
+  filteredPlaylists: computed('playlists', 'playlist', function () {
+    if (this.get('filterOutSelectedPlaylists') && this.get('playlist')) {
+      this.get('playlists').rejectBy('id', this.get('playlist.id'));
+    }
+    return this.get('playlists');
+  })
 });
